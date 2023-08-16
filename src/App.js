99e0a5d3f0b0f24dc78f8a7976231f50b87747ad
Lurie <lurainekimmerle@gmail.com>
2023-08-16 05:00:55 +0000

    add jokes values to App.js as attribute

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Jokes from "./Jokes.js"
 export default function App() {
   return (
     <div className="App">
-      <Jokes/>
-      <Jokes/>
-      <Jokes/>
-      <Jokes/>
+            <Jokes 
+        setup="Why don't programmers like nature?"
+        punchline="It has too many bugs."
+      />
+      <Jokes 
+        setup="Why did the React component feel trapped?"
+        punchline="Because it was stuck in a state."/>
+      <Jokes 
+        setup="How do you comfort a JavaScript bug"
+        punchline="You console it!"
+      />
+      <Jokes 
+        setup="Frontend development is like a painting that's never quite finished. Just when you think it's perfect, someone finds a spot you missed... in Internet Explorer."
+      />
     </div>
   );
 }
