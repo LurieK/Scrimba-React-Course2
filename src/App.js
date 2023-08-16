5b6d7d8be8983b1d6e093a3cef4bfacc38c743d5
Lurie <lurainekimmerle@gmail.com>
2023-08-16 05:18:37 +0000

    add isPun variable to Jokes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ export default function App() {
             <Jokes 
         setup="Why don't programmers like nature?"
         punchline="It has too many bugs."
+        isPun={true}
       />
       <Jokes 
         setup="Why did the React component feel trapped?"
-        punchline="Because it was stuck in a state."/>
+        punchline="Because it was stuck in a state."
+        isPun={true}
+        />
       <Jokes 
         setup="How do you comfort a JavaScript bug"
         punchline="You console it!"
+        isPun={true}
       />
       <Jokes 
         setup="Frontend development is like a painting that's never quite finished. Just when you think it's perfect, someone finds a spot you missed... in Internet Explorer."
+        isPun={false}
       />
     </div>
   );
