0d13adb75e053b26c69a3815b1aa1ca86da0aa2d
Lurie <lurainekimmerle@gmail.com>
2023-08-17 03:23:47 +0000

    create object file, and map through to return jokes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import "./styles.css";
 import Jokes from "./Jokes.js"
+import jokesData from "./JokesData"
 
 export default function App() {
+  const jokeElements= jokesData.map((joke)=>{
+    return <Jokes 
+          setup={joke.setup}
+          punchline={joke.punchline}
+          inPun={joke.isPun}
+    
+    />
+  })
+
   return (
-    <div className="App">
-            <Jokes 
-        setup="Why don't programmers like nature?"
-        punchline="It has too many bugs."
-        isPun={true}
-      />
-      <Jokes 
-        setup="Why did the React component feel trapped?"
-        punchline="Because it was stuck in a state."
-        isPun={true}
-        />
-      <Jokes 
-        setup="How do you comfort a JavaScript bug"
-        punchline="You console it!"
-        isPun={true}
-      />
-      <Jokes 
-        setup="Frontend development is like a painting that's never quite finished. Just when you think it's perfect, someone finds a spot you missed... in Internet Explorer."
-        isPun={false}
-      />
-    </div>
+    <div>
+      {jokeElements}
+      </div>
   );
 }
---
+++ src/JokesData.js
@@ -0,0 +1,23 @@
+const jokesData = [
+  {
+    setup: "Why don't programmers like nature?",
+    punchline: "It has too many bugs.",
+    isPun: true
+  },
+  {
+    setup: "Why did the React component feel trapped?",
+    punchline: "Because it was stuck in a state.",
+    isPun: true
+  },
+  {
+    setup: "How do you comfort a JavaScript bug",
+    punchline: "You console it!",
+    isPun: true
+  },
+  {
+    setup: "Frontend development is like a painting that's never quite finished. Just when you think it's perfect, someone finds a spot you missed... in Internet Explorer.",
+    isPun: false
+  }
+];
+
+export default jokesData;
